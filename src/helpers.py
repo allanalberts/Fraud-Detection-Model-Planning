3040696e95ef18070db0f03316b29366b11658d5
allanalberts <allan.alberts@gmail.com>
2020-11-01 20:29:08 -0800

    pep8 code clean-up

--- src/helpers.py
+++ src/helpers.py
@@ -2,6 +2,7 @@ import scipy.stats as stats
 import pandas as pd
 import numpy as np
 
+
 def load_data(datafile):
     """
     Returns an pandas dataframe containg the source data and a list containg the fraud features to be analyzed.
@@ -51,7 +52,6 @@ def bootstrap_mean(df, statistic, samples=500):
     return df_bootstrap.mean()
 
 
-
 def ttest(sample1, sample2, Features):
     """
     Returns a dataframe with a single row containing the p values for each Feature when sample1
@@ -70,24 +70,25 @@ def ttest(sample1, sample2, Features):
 
     p_lst = []
     for f in Features:
-        stat, p_val = stats.ttest_ind(sample1[f], sample2[f], equal_var=False)  
+        stat, p_val = stats.ttest_ind(sample1[f], sample2[f], equal_var=False)
         p_lst.append(p_val)
     df = pd.DataFrame(p_lst, index=Features).reset_index()
-    df.columns = ['Feature','p_val']
+    df.columns = ['Feature', 'p_val']
     return df
+
 
 def print_fraud_rate(TranAmt, data, cummulative=False):
     '''
         Prints the fraud rates for specific price points
-        
+
         ARGS:
             TranAmt -> dollar value to rate analysis
             data -> pandas df
             cummulative - boolean value if rate should include all values below TranAmt
-           
+
         Return:
             None
-    '''    
+    '''
 
     if TranAmt == 0:
         print(f"Overall Fraud rate: {100 * data['Class'].mean():0.2f}%")
@@ -102,53 +103,55 @@ def print_fraud_rate(TranAmt, data, cummulative=False):
 def relevant_features(fraud_sample, legit_sample, Features, p_threshold):
     '''
         Creates a list of relevant features based on p-value vs p-threshold
-        
+
         ARGS:
             fraud_sample - pandas dataframe of fraud transactions to analyze
             legit_sample - pandas dataframe of non-fraud transactions to analyze
             Features - list of features to analyze
             p_threshold - number to compare p-value against
-            
+
         Return:
             list of features
     '''
     low_p_features = []
     for f in Features:
-        stat, p_val = stats.ttest_ind(fraud_sample[f], legit_sample[f], equal_var=False) 
+        stat, p_val = stats.ttest_ind(fraud_sample[f], legit_sample[f], equal_var=False)
         if p_val < p_threshold:
             low_p_features.append(f)
     return low_p_features
 
+
 def print_recommended_features(data, Features, amt_threshold, p_threshold):
     '''
         Prints a list of relevant features for fraud models
-        
+
         ARGS:
             data - pandas dataframe containing dataset to be analyzed
             Features - list of features
             amt_threshold - dollar amount used to segment the data for analysis
-            p_threshold - number used to specify p-value limit for identifying relevant features
+            p_threshold - number used to specify p-value limit for
+                          identifying relevant features
 
         Return:
             None
     '''
-    fraud_sample_low = data[(data['Class']==1) & (data['Amount']<=amt_threshold)]
-    legit_sample_low = data[(data['Class']==0) & (data['Amount']<=amt_threshold)]
-    fraud_sample_high = data[(data['Class']==1) & (data['Amount']>amt_threshold)]
-    legit_sample_high = data[(data['Class']==0) & (data['Amount']>amt_threshold)]
-    
+    fraud_sample_low = data[(data['Class'] == 1) & (data['Amount'] <= amt_threshold)]
+    legit_sample_low = data[(data['Class'] == 0) & (data['Amount'] <= amt_threshold)]
+    fraud_sample_high = data[(data['Class'] == 1) & (data['Amount'] > amt_threshold)]
+    legit_sample_high = data[(data['Class'] == 0) & (data['Amount'] > amt_threshold)]
 
     low_amt_f = relevant_features(fraud_sample_low, legit_sample_low, Features, p_threshold)
     high_amt_f = relevant_features(fraud_sample_high, legit_sample_high, Features, p_threshold)
-    
+
     both = [x for x in low_amt_f if x in high_amt_f]
     low_only = [x for x in low_amt_f if x not in high_amt_f]
-    high_only = [x for x in high_amt_f if x not in low_amt_f]                    
-                        
+    high_only = [x for x in high_amt_f if x not in low_amt_f]
+
     print(f'Relevenant Features for Both models: \r\n{both}')
     print(f'\nAdditional Features for Low Amount model only: {low_only}')
     print(f'\nAdditional Features for High Amount model only: {high_only}')
     return None
+
 
 if __name__ == '__main__':
     data, Features = load_data("../data/creditcard.csv")
@@ -158,4 +161,4 @@ if __name__ == '__main__':
     print_fraud_rate(1.00, data)
     print_fraud_rate(1.00, data, cummulative=True)
 
-    print_recommended_features(data, Features, amt_threshold=1.00, p_threshold=0.01)    
+    print_recommended_features(data, Features, amt_threshold=1.00, p_threshold=0.01)
--- src/plot.py
+++ src/plot.py
@@ -6,14 +6,17 @@ import matplotlib.ticker as ticker
 import matplotlib.ticker as mtick
 import matplotlib.gridspec as gs
 
+
 def plot_pval_for_model(ax, grp1, grp2, Features, title):
     '''
-        Prints individual plot of p-values for each feature. 
-        
+        Prints individual plot of p-values for each feature.
+
         ARGS:
             ax - axis to print on
-            grp1 - pandas dataframe containing sample of dataset for ttest comparison
-            grp2 - pandas dataframe containing sample of dataset for ttest comparison
+            grp1 - pandas dataframe containing sample of
+                   dataset for ttest comparison
+            grp2 - pandas dataframe containing sample of
+                   dataset for ttest comparison
             Features - features to plot
             title - plot title
 
@@ -22,87 +25,90 @@ def plot_pval_for_model(ax, grp1, grp2, Features, title):
     '''
     df = h.ttest(grp1, grp2, Features)
     y = df['p_val']
-    ax.bar(Features,y)
+    ax.bar(Features, y)
     ax.set_ylim(0, 0.05)
     ax.set_ylabel("p-value")
     ax.set_xlabel("Features")
     ax.set_title(title)
 
+
 def plot_pval_all(data, amount_threshold):
     '''
         Prints multiple plots of p-values for each feature
-        
+
         ARGS:
             data - pandas dataframe containing dataset
             amount_threshold - dollar amount for segmenting comparison groups
-                    
+
         Return:
             None
     '''
-    fig, axs = plt.subplots(2,1,figsize=(14,4))
-    grp1 = data[(data['Class']==1) & (data['Amount']<=amount_threshold)]
-    grp2 = data[(data['Class']==0) & (data['Amount']<=amount_threshold)]
-    title = f'Feature p-value (fraud vs. non-fraud) for transactions <= ${amount_threshold}'
+    fig, axs = plt.subplots(2, 1, figsize=(14, 4))
+    grp1 = data[(data['Class'] == 1) & (data['Amount'] <= amount_threshold)]
+    grp2 = data[(data['Class'] == 0) & (data['Amount'] <= amount_threshold)]
+    title = f'Feature p-value (fraud vs. non-fraud) for amount <= ${amount_threshold}'
     plot_pval_for_model(axs[0], grp1, grp2, Features, title)
 
-    grp3 = data[(data['Class']==1) & (data['Amount']>amount_threshold)]
-    grp4 = data[(data['Class']==0) & (data['Amount']>amount_threshold)]
-    title = f'Feature p-value (fraud vs. non-fraud) for transactions > ${amount_threshold}'
+    grp3 = data[(data['Class'] == 1) & (data['Amount'] > amount_threshold)]
+    grp4 = data[(data['Class'] == 0) & (data['Amount'] > amount_threshold)]
+    title = f'Feature p-value (fraud vs. non-fraud) for amount > ${amount_threshold}'
     plot_pval_for_model(axs[1], grp3, grp4, Features, title)
 
     plt.tight_layout()
     fig.savefig("../img/features_pval.png")
 
+
 def plot_dataset_overview(data):
     '''
-        Creates a timeseries image of the dataset showing fraud and legitimate transaction trends
-                
+        Creates a timeseries image of the dataset showing
+        fraud and legitimate transaction trends
+
         ARGS:
             data - pandas dataframe
-            
+
         Return:
             None
-    '''    
-    trends = data[['Time','Amount','Class']].copy()
+    '''
+    trends = data[['Time', 'Amount', 'Class']].copy()
     trends['TimeSeries'] = pd.to_datetime(trends.Time, unit='s', origin='2013-09-01')
     trends.set_index('TimeSeries', drop=False, inplace=True)
 
-    fraud = trends[(trends.Class==1)][['Amount']].resample('H').count().dropna()
+    fraud = trends[(trends.Class == 1)][['Amount']].resample('H').count().dropna()
     fraud.columns = ['Trans_cnt']
-    legit = trends[(trends.Class==0)][['Amount']].resample('H').count().dropna()
+    legit = trends[(trends.Class == 0)][['Amount']].resample('H').count().dropna()
     legit.columns = ['Trans_cnt']
-    
-    fig, ax = plt.subplots(figsize = (12,4))
+
+    fig, ax = plt.subplots(figsize=(12, 4))
     fraud['Trans_cnt'].plot(color='r', kind='area', alpha=0.5, label='Fraud Transactions')
     legit['Trans_cnt'].plot(secondary_y=True, color='b', label='Sales Transactions')
-    ax.set_ylabel('Fraud Count', color='r');
+    ax.set_ylabel('Fraud Count', color='r')
     ax.tick_params(axis='y', colors='red')
-    plt.ylabel('Sales Count', color='b');
+    plt.ylabel('Sales Count', color='b')
     plt.yticks(color='b')
     plt.axvline(x='2013-09-02 00:00:00', color='black', linestyle='dashed')
-    plt.legend(frameon=False, loc='upper right');
-    plt.title('Transaction Counts by hour', color='black');
+    plt.legend(frameon=False, loc='upper right')
+    plt.title('Transaction Counts by hour', color='black')
     plt.savefig('intro_fig.png')
 
 
 def graph_amount_cdf(ax, x, y1, y2, ymax):
     '''
         Creates plot representing the cdf of the fraud amounts
-        
+
         ARGS:
             ax - plot axis
             x - list of features names to plot against
             y1 - list of cummulative amounts for fraud transactions
             y2 - list containing cdf values for fraud transactions
             ymax - the upper limit for y axis on plot
-           
+
         Return:
            ax - plot axis
-    '''  
-    
-    percent =  ticker.FormatStrFormatter('%1.0f')
+    '''
+
+    percent = ticker.FormatStrFormatter('%1.0f')
     currency = mtick.StrMethodFormatter('${x:,.0f}')
-    
+
     ax.plot(x, y1, color='red', label="Cummulative Fraud Loss")
     ax.xaxis.set_major_formatter(currency)
     ax.set_xlabel("Transaction Amount")
@@ -114,86 +120,89 @@ def graph_amount_cdf(ax, x, y1, y2, ymax):
 
     ax.set_xlim(0, right=None)
     ax.set_ylim(0, ymax)
-    ax_2 = ax.twinx() 
+    ax_2 = ax.twinx()
     ax_2.plot(x, y2, color='blue', label="Percent of Fraud Transactions")
     ax_2.set_ylabel("Percent of Fraud Transactions", color="blue")
     ax_2.yaxis.set_major_formatter(percent)
     ax_2.tick_params(axis='y', colors='blue')
-    ax_2.set_ylim(0,100)
+    ax_2.set_ylim(0, 100)
 
     plt.title("Cummulative Fraud Losses and Transactions", fontsize=20)
     plt.axvline(x=100, color='green', linestyle='dashed', linewidth=1, label='$100')
     plt.legend(loc='lower center', frameon=False)
-    plt.tight_layout() 
-    
+    plt.tight_layout()
+
+
 def amount_cdf_data(data):
     '''
         Creates 3 lists used for plotting fraud eda chart
-        
+
         ARGS:
             data -> pandas df
-           
+
         Return:
             x - list of features names to plot against
             y1 - list of cummulative amounts for fraud transactions
             y2 - list of cdf values for fraud transactions
-    '''  
+    '''
     max_amt = data['Amount'].max()
     transaction_cnt = data['Amount'].count()
-    x = [n for n in range(1,int(max_amt),int(max_amt/100))]
+    x = [n for n in range(1, int(max_amt), int(max_amt/100))]
     y1 = []
     y2 = []
     for n in range(102):
-        y1.append(data[data['Amount']<(.01*n*max_amt)]['Amount'].sum())
-        y2.append(((data[data['Amount']<(.01*n*max_amt)]['Amount'].count())/transaction_cnt)*100)
+        y1.append(data[data['Amount'] < (.01*n*max_amt)]['Amount'].sum())
+        y2.append(((data[data['Amount'] < (.01*n*max_amt)]['Amount'].count())/transaction_cnt)*100)
     ymax = np.max(y1)
     return x, y1, y2, ymax
+
 
 def graph_amounts_frequency(ax, data, xlimit):
     '''
         Creates plots showing frequency of fraud at different transaction amounts
-        
+
         ARGS:
             ax - plot axis
             y2 - list of cdf values to plot
             data - pandas df
             xlimit - x-axis graph upper limit
-           
+
         Return:
             ax - axis with plot
-    '''    
+    '''
     transaction_cnt = data['Amount'].count()
     y2 = []
     for n in range(102):
-        y2.append(((data[data['Amount']<(.01*n*xlimit)]['Amount'].count())/transaction_cnt)*100)
-    
-    gr_data = data[data['Class']==1].copy()
-    gr_data[gr_data['Amount']<xlimit]['Amount'].hist(ax=ax, bins=100, color='red', alpha=0.7)
+        y2.append(((data[data['Amount'] < (.01*n*xlimit)]['Amount'].count())/transaction_cnt)*100)
+
+    gr_data = data[data['Class'] == 1].copy()
+    gr_data[gr_data['Amount'] < xlimit]['Amount'].hist(ax=ax, bins=100, color='red', alpha=0.7)
     ax.set_title("Frequency of Fraud by Transaction Dollar Amount (0-100)", fontsize=20)
     ax.set_xlabel('Fraud Transaction Amount')
-    ax_2 = ax.twinx() 
+    ax_2 = ax.twinx()
     ax_2.plot(y2)
     currency = mtick.StrMethodFormatter('${x:,.0f}')
     ax.xaxis.set_major_formatter(currency)
     ax.set_ylabel('Transactions')
-    ax.set_xlim(-1,101)
+    ax.set_xlim(-1, 101)
     ax.set_facecolor('lightgreen')
+
 
 def plot_fraud_amount_eda(data, gs):
     '''
         Creates image with multiple plots for Fraud Amount EDA Visualatioin
-        
+
         ARGS:
             data - pandas df
             gs - instantiated Gridspec
-           
+
         Return:
             None
-    '''    
-    df_fraud = data[data['Class']==1]
-    df_legit = data[data['Class']==0]
+    '''
+    df_fraud = data[data['Class'] == 1]
+    df_legit = data[data['Class'] == 0]
 
-    fig = plt.figure(figsize=(20,6))
+    fig = plt.figure(figsize=(20, 6))
     ax1 = fig.add_subplot(gs[0:-1])
 
     x, y1, y2, ymax = amount_cdf_data(df_fraud)
@@ -206,7 +215,6 @@ def plot_fraud_amount_eda(data, gs):
     fig.savefig("../img/AmountPlots.png")
 
 
-
 if __name__ == '__main__':
     data, Features = h.load_data("../data/creditcard.csv")
 
@@ -214,7 +222,7 @@ if __name__ == '__main__':
     plot_pval_all(data, amount_threshold=1.00)
 
     # create the fraud amount eda charts: AmountPlots.png
-    gs = gs.GridSpec(1,2)    
+    gs = gs.GridSpec(1, 2)
     plot_fraud_amount_eda(data, gs)
 
     # create plot of dataset overview
